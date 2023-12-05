f194056bb0c3e58999ead3850d9cdd6ab5aaa0c6
Loise College <144301256+loisem444@users.noreply.github.com>
2023-12-05 13:24:53 -0500

    Style details screen and splash screen

--- app/src/main/java/com/example/emberlinkand/MainActivity.java
+++ app/src/main/java/com/example/emberlinkand/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.emberlinkand;
 
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,38 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button CreateEventBtn = findViewById(R.id.CreateEventButton);
-        Button DashboardButton = findViewById(R.id.DashBoardButton);
-        Button EventListButton = findViewById(R.id.EventListButton);
-
-
-
-        CreateEventBtn.setOnClickListener(new View.OnClickListener() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                // Start SecondActivity
-                Intent intent = new Intent(MainActivity.this, CreateEventActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        EventListButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start SecondActivity
-                Intent intent = new Intent(MainActivity.this, EventListActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        DashboardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start SecondActivity
+            public void run() {
                 Intent intent = new Intent(MainActivity.this, DashBoardActivity.class);
                 startActivity(intent);
+                finish();
             }
-        });
-
+        }, 3000);
     }
 }
