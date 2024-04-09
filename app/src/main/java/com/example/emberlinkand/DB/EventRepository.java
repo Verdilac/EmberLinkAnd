76bca799a43c48c3be8580726716a723dd3c1555
Pinto-PINTO <marie334fe@gmail.com>
2024-04-09 16:54:51 -0400

    Added Event Count Method

--- app/src/main/java/com/example/emberlinkand/DB/EventRepository.java
+++ app/src/main/java/com/example/emberlinkand/DB/EventRepository.java
@@ -10,17 +10,23 @@ class EventRepository {
 
     private EventDao eventDao;
     private LiveData<List<Event>> allEvents;
+    private LiveData<Integer> eventCount;
 
     EventRepository(Application application) {
         AppDatabase db = AppDatabase.getDbInstance(application);
         eventDao = db.eventDao();
         allEvents = eventDao.getAllEvents();
+        eventCount = eventDao.getEventCount();
     }
 
     // Room executes all queries on a separate thread.
     // Observed LiveData will notify the observer when the data has changed.
     LiveData<List<Event>> getAllEvents() {
         return allEvents;
+    }
+
+    public LiveData<Integer> getEventCount() {
+        return eventCount;
     }
 
     LiveData<Event> getByEventID(int eventID) {
